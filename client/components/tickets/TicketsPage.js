f5e911a44378ac29ec1afd4ded4285b747e726d8
rnavyaprabha <navyaprabha.rajappa@gmail.com>
2025-11-04 23:14:11 -0500

    fix: Remove duplicate imports in TicketsPage component
    - Eliminated redundant import statements for formatTime, calculateTotalTime, and formatDurationText from TimeUtils.js, streamlining the code and improving maintainability.

--- client/components/tickets/TicketsPage.js
+++ client/components/tickets/TicketsPage.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Teams, Tickets, ClockEvents } from '../../../collections.js';
 import { currentTime } from '../layout/MainLayout.js';
 import { formatTime, calculateTotalTime, formatDurationText } from '../../utils/TimeUtils.js';
+import { formatTime, calculateTotalTime, formatDurationText } from '../../utils/TimeUtils.js';
 import { extractUrlTitle } from '../../utils/UrlUtils.js';
 import { getUserTeams } from '../../utils/UserTeamUtils.js';
 
