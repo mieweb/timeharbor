f11f27e998883f06b348ab2201ba6eacaa7ee858
Aditya Damerla <75409196+zesty-genius128@users.noreply.github.com>
2025-11-15 16:40:15 -0500

    refactor: Remove Ozwell integration collections from collections.js

--- collections.js
+++ collections.js
@@ -5,8 +5,3 @@ export const Teams = new Mongo.Collection('teams');
 export const Sessions = new Mongo.Collection('sessions');
 
 export const ClockEvents = new Mongo.Collection('clockevents');
-
-// Ozwell Integration Collections
-export const OzwellWorkspaces = new Mongo.Collection('ozwellworkspaces');
-export const OzwellUsers = new Mongo.Collection('ozwellusers');
-export const OzwellConversations = new Mongo.Collection('ozwellconversations');
